8311d3724bf6aa18ca06acda394755fc39411c69
Maria Shevchenko <shevtma@yandex.ru>
2022-10-26 01:07:29 +0300

    added script task_score.js

---
+++ task_score.js
@@ -0,0 +1,6 @@
+let scoreModuleIntro = 100;
+let scoreModuleGit = 115;
+let scoreModuleJs = 120;
+let fullName = 'Иван Иванов'
+let averageScore = (scoreModuleIntro+scoreModuleGit+scoreModuleJs)/3;
+console.log(`Средний балл студента ${fullName} равен ${averageScore} баллов.`);
