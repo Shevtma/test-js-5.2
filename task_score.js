c58cc50e701f14e8466e10b15d5c428440c58b53
Maria Shevchenko <shevtma@yandex.ru>
2022-10-28 00:34:34 +0300

    added toFixed() method for averageScore

--- task_score.js
+++ task_score.js
@@ -3,4 +3,4 @@ let scoreModuleGit = 115;
 let scoreModuleJs = 120;
 let fullName = 'Иван Иванов'
 let averageScore = (scoreModuleIntro+scoreModuleGit+scoreModuleJs)/3;
-console.log(`Средний балл студента ${fullName.slice(0,1)} равен ${averageScore} баллов.`);
+console.log(`Средний балл студента ${fullName.slice(0,1)} равен ${averageScore.toFixed(2)} баллов.`);
