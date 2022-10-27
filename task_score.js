93633eac0c8d96185e50bf2d26f68cd31c62a576
Maria Shevchenko <shevtma@yandex.ru>
2022-10-28 00:39:33 +0300

    added studentName and studentSurname separately

--- task_score.js
+++ task_score.js
@@ -1,6 +1,7 @@
 let scoreModuleIntro = 100;
 let scoreModuleGit = 115;
 let scoreModuleJs = 120;
-let fullName = 'Иван Иванов'
+let studentName = 'Иван'
+let studentSurname = 'Иванов'
 let averageScore = (scoreModuleIntro+scoreModuleGit+scoreModuleJs)/3;
-console.log(`Средний балл студента ${fullName.slice(0,1)} равен ${averageScore.toFixed(2)} баллов.`);
+console.log(`Средний балл студента ${studentName.slice(0,1)}. ${studentSurname} равен ${averageScore.toFixed(2)} баллов.`);
